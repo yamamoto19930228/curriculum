fa6e286e1d7af092bc6d0e824a60f017e1ce4a37
yamamoto19930228 <83966657+yamamoto19930228@users.noreply.github.com>
2021-06-01 14:49:56 +0900

    Update Task1_4.java

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -45,12 +45,12 @@ public class Task1_4 {
 
         // ③ 「USER_NAME」の値のみ等しい場合。
        //     定数を使用して「 パスワードに誤りがあります。 」 と出力してください。
-        else if (name.equals(USER_NAME) && pass != USER_PASSWORD) {
+        else if (name.equals(USER_NAME) && !(pass.equals(USER_PASSWORD))) {
             System.out.println(CONST_MSG_ERROR_PASS);
         }
         
         // ④ 「USER_PASSWORD」の値のみ等しい場合。
-        else if (name != USER_NAME && pass.equals(USER_PASSWORD)) {
+        else if (!(name.equals(USER_NAME)) && pass.equals(USER_PASSWORD)) {
             System.out.println(CONST_MSG_ERROR_NAME);
         }
        //定数を使用して「 名前に誤りがあります。 」と出力してください。
