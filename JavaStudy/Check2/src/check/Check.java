bb8b9e59d1fb02e4c93750ba186a2c4784524555
yamamoto19930228 <83966657+yamamoto19930228@users.noreply.github.com>
2021-07-29 12:28:23 +0900

    2章チェックテスト　修正

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,17 +1,24 @@
 package check;
 
+import constants.Constants;
+
+
 class Check {
 	private static String firstName = "啓稔";
 	private static String lastName = "山本";
 	
 	public static void main(String[]args){
-		System.out.println("printNameメソッド → " + lastName + firstName);
-		Pet javakichi = new  Pet("java吉", "hoge");
+		System.out.println("printNameメソッド → " + getName(lastName, firstName));
+		//クラスpetのインスタンスjavakichiを生成してメソッドintroduceを呼び出す
+		Pet javakichi = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
 		javakichi.introduce();
 		System.out.println();
-		RobotPet robot = new  RobotPet("R2D2", "ルーク");
+		RobotPet robot = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
 		robot.introduce();
 		System.out.println();
 	}
 	
+	public static String getName(String lastName , String firstName) {
+		return lastName + firstName;
+	}
 }
--- JavaStudy/Check2/src/check/Pet.java
+++ JavaStudy/Check2/src/check/Pet.java
@@ -1,15 +1,15 @@
 package check;
 
 public class Pet {
-
+//フィールド
     private String name;
     private String masterName;
-
+//コンストラクタ
     public Pet(String name, String masterName) {
         this.name = name;
         this.masterName = masterName;
     }
-
+//メソッドが3つ
     protected String getName() {
         return name;
     }
